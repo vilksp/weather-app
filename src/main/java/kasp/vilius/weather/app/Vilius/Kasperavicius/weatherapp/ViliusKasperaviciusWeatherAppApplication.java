41c869dac48cb4088c484a57f30367a08768acb6
vilksp <v.kasperavicius1@gmail.com>
2020-06-06 14:09:33 +0300

    initial commit

---
+++ src/main/java/kasp/vilius/weather/app/Vilius/Kasperavicius/weatherapp/ViliusKasperaviciusWeatherAppApplication.java
@@ -0,0 +1,13 @@
+package kasp.vilius.weather.app.Vilius.Kasperavicius.weatherapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViliusKasperaviciusWeatherAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViliusKasperaviciusWeatherAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kasp/vilius/weather/app/Vilius/Kasperavicius/weatherapp/ViliusKasperaviciusWeatherAppApplicationTests.java
@@ -0,0 +1,13 @@
+package kasp.vilius.weather.app.Vilius.Kasperavicius.weatherapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViliusKasperaviciusWeatherAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
